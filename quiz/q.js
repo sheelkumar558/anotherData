10bcddccac72238ceaa615c542e9718e321c9e0f
Sheelu verma <97446106+sheelkumar558@users.noreply.github.com>
2022-09-17 15:06:30 +0530

    done

--- quiz/q.js
+++ quiz/q.js
@@ -1,13 +1,57 @@
-'use strict'
-function name(){
-    var name = 'masi';
-    console.log(this.name);
-}
-function greet(){
-    console.log("hello");
-}
-var arr = [{name:"n"},{name:"A"},{name:"Ab"}];
-for(let i=0;i<arr.length;i++){
-    setTimeout(name.call(arr[i]),0);
-    setTimeout(greet(),0)
-}
+// 'use strict'
+// function name(){
+//     var name = 'masi';
+//     console.log(this.name);
+// }
+// function greet(){
+//     console.log("hello");
+// }
+// var arr = [{name:"n"},{name:"A"},{name:"Ab"}];
+// for(let i=0;i<arr.length;i++){
+//     setTimeout(name.call(arr[i]),0);
+//     setTimeout(greet(),0)
+// }
+
+// var arr =[1,2,3,4,5,6];
+// var out = arr.filter((el)=>{
+//     return el%3==0;
+// }).reduce((ac,el)=>{
+//     return ac+el**3;
+// },1);
+// console.log(out);
+
+// var c=0;
+// function sum(a,b){
+//     var c=10;
+//     return a+b+c;
+// }
+// console.log(sum(5,5));
+
+// var arr = [0,0,0,0];
+// console.log(arr.filter((el)=>{
+//     return el;
+// }));
+// var f1= function f2(){
+//     console.log("clg");
+// }
+// f1()
+// f2()
+// for (var index = 0; index < 10; index++) {
+//    // const element = array[index];
+    
+// }
+// console.log(index);
+
+
+var arr = [1];
+arr.reduce((a,b)=>{
+    console.log(a);
+    console.log(b);
+})
+
+
+
+
+
+
+
